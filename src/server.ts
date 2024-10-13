bc4b30f65b0f07fe8d45ea44e7978871e87072e8
Moshe <moskno1@gmail.com>
2024-10-13 11:42:10 +0300

    removing node modules and inserting it to .gitignore

---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes";
+import classRoutes from "./routes/classRoutes";
+import studentRoutes from "./routes/studentRoutes";
+import teacherRoutes from "./routes/teacherRoutes";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "School Management API",
+      version: "1.0.0",
+      description: "API for managing grades of students",
+    },
+    servers: [
+      {
+        url: "http://localhost:3500",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts"],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use("/auth", authRoutes);
+app.use("/users", classRoutes);
+app.use("/users", studentRoutes);
+app.use("/users", teacherRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
